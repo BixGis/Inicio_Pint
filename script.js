eda28a3713d4b036797484df7d4880c5348ca1b0
BixGis <gustavoayavaca2003@gmail.com>
2025-02-10 00:28:50 -0500

    Version1.2

---
+++ script.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeModal = document.querySelector('.close');
+
+document.querySelectorAll('.gallery .card img').forEach((img) => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        modalImg.src = img.src;
+    });
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
